c5b9e195bf8506cb0b89cfc74636349dd655eb41
Oladimeji Ibitoye <stephen_ibitoye@yahoo.com>
2023-01-05 19:40:34 +0100

    set up knex.js

--- knexfile.js
+++ knexfile.js
@@ -6,38 +6,12 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user:     process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD
     },
     migrations: {
       tableName: 'knex_migrations'
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KnexModule } from 'nest-knexjs';
 
 @Module({
-  imports: [],
+  imports: [
+    KnexModule.forRoot({
+      config:{
+        client: 'pg',
+        connection: {
+          host: process.env.DATABASE_HOST,
+          user: process.env.DATABASE_USERNAME,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+          port: Number(process.env.DATABASE_PORT)
+        },
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
