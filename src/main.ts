50e33e9cd5636d26840d79e1dcdefb76501ca0c3
Oladimeji Ibitoye <stephen_ibitoye@yahoo.com>
2023-01-05 23:25:04 +0100

    set up swagger openapi for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
+
+  const options = new DocumentBuilder()
+    .setTitle('Meal Management App')
+    .setDescription('A backend API for a meal management application that allows users to create, read, update, and delete meal addons')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(Number(process.env.PORT) || 3000);
 }
 bootstrap();
